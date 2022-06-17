00785225b51fef60ce7449a454ac8aaf345a94a1
TheDarkBatman <87569239+TheDarkBatman2@users.noreply.github.com>
2022-06-17 15:33:05 +0430

    Made movement better

--- Untitled/Assets/Scripts/Libraries/References.cs
+++ Untitled/Assets/Scripts/Libraries/References.cs
@@ -7,6 +7,7 @@ public class References : MonoBehaviour
     public static References Instance { get; private set; }
 
     // Put your global values in here
+    public Transform cameraTransform;
 
     private void Awake() {
         Instance = this;
