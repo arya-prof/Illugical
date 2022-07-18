292daf1b767bd6ec04dc050945ad117508ee1f9a
TheDarkBatman <87569239+TheDarkBatman2@users.noreply.github.com>
2022-07-18 18:36:34 +0430

    Added quest to lvl 0 and made checklist for level 1 and 0

--- Untitled/Assets/Scripts/MonoBehaviours/Puzzle/Quests/QuestHandler.cs
+++ Untitled/Assets/Scripts/MonoBehaviours/Puzzle/Quests/QuestHandler.cs
@@ -10,12 +10,14 @@ public class QuestHandler : MonoBehaviour
     [SerializeField] private Quest _quest;
 
     private void Awake() {
+        checkBox.SetActive(false);
         _quest.triggerEvent = new UnityEngine.Events.UnityEvent();
         _quest.triggerEvent.AddListener(FinishQuest);
         description.text = _quest.description;
     }
 
     private void FinishQuest() {
+        checkBox.SetActive(true);
         Debug.Log("Quest is finished!");
     }
 }
