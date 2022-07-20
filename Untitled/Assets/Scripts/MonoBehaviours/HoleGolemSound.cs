275ff5f412cdce6a2d1066f4fc90c4bb2d4d0f0d
Arya Valizadeh <arya.valizadeh@outlook.com>
2022-07-20 23:09:09 +0430

    add golem sound effect+camera shake in lvl_2

---
+++ Untitled/Assets/Scripts/MonoBehaviours/HoleGolemSound.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+public class HoleGolemSound : MonoBehaviour
+{
+    public AudioClip impact;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void HgolemSound()
+    {
+            audioSource.PlayOneShot(impact, 1f);
+            CameraShaker.Instance.ShakeOnce(4f, 4f, 0.1f, 1f);
+    }
+}
